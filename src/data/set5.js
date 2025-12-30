84f9a3dce256c45132c3a1cc22a92009e7c0c44e
Dean Compton <deancompton194@yahoo.com>
2025-12-30 16:27:27 -0600

    new boards

---
+++ src/data/set5.js
@@ -0,0 +1,7 @@
+export const set4 = [
+  { number: 1, id: "nfl_rookiepassyds", title: "NFL Most Pass Yds by a Rookie", category: "NFL Rookie" },
+  { number: 2, id: "nfl_rookierecyds", title: "NFL Most Rec Yds by a Rookie", category: "NFL Rookie" },
+
+  { number: 3, id: "nfl_terecyds2000", title: "NFL Most Rec Yds by a TE since 2000", category: "NFL Rec" },
+  { number: 4, id: "nb_ppglefties2000", title: "NBA Most PPG by a Lefty since 2000", category: "NBA PPG" },
+];
